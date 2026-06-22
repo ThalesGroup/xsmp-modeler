ee311c3774ee6373e89bd7b0642d5b6cd1a9254f
Julien Boldrini <julien.boldrini@thalesaleniaspace.com>
2026-06-22 15:34:05 +0200

    improve

--- packages/xsmp-tas-mdk/test/expected/generator-python/helpers/builder/demo/support/MonitorService/user_code.py
+++ packages/xsmp-tas-mdk/test/expected/generator-python/helpers/builder/demo/support/MonitorService/user_code.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 def configureInstance(jsim, instance_data):
     pass
--- packages/xsmp-tas-mdk/test/expected/generator-python/helpers/test_utils/demo/support/MonitorService/__init__.py
+++ packages/xsmp-tas-mdk/test/expected/generator-python/helpers/test_utils/demo/support/MonitorService/__init__.py
@@ -1,3 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from .wrapper import MonitorServiceWrapper
